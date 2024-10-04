23a169cda4c8d658b007c1f9ba91ca8795b06b59
BillyBat04 <121949115+BillyBat04@users.noreply.github.com>
2024-10-04 15:36:15 +0700

    PMWeb
    source

---
+++ PMWeb/PMWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PMWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
